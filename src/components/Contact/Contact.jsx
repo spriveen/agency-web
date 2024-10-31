58782924727f6e8db30312b273074ed8713fc71e
spriveen <rpeanuka@gmail.com>
2024-10-30 21:52:59 -0700

    update contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <div className="min-h-screen container flex flex-col lg:flex-row justify-center lg:justify-between items-center">
         <div data-aos="fade-up" className=" w-full lg:w-2/4">
           <h1 className=" text-4xl lg:text-6xl font-semibold leading-tight">
-            Ready to start your project?
+          Lets make your project a reality!
           </h1>
           <div className=" border-b-2 border-black pb-3 mt-10">
             <input
@@ -36,10 +36,10 @@ const Contact = () => {
           className=" w-full lg:w-1/3 space-y-8 mt-14 lg:mt-0"
         >
           <p className=" text-xl lg:text-2xl font-medium">
-            We carry out the process to achieve maximum growth of your business
+          We guide your business toward peak performance and expansion
           </p>
           <button className=" bg-tertiary px-6 py-2 shadow-[5px_5px_0px_0px_rgba(0,0,0)] rounded-md cursor-pointer">
-            Book a Call
+          Arrange a Consultation
           </button>
         </div>
       </div>
